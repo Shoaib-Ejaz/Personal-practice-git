4f8f47f0d6fc58b6e9b792b45fa8de66c97d7e7d
Shoaib Ejaz <308232@ust.com>
2026-03-24 20:34:38 +0530

    added a function

--- prog1.java
+++ prog1.java
@@ -1,6 +1,15 @@
-int x=5;
-int y=1;
-public static int sub()
-{
-    return x-y;
+public class prog1 {
+    int x = 5;
+    int y = 1;
+
+    public static int sub(int x,int y) {
+        return x - y;
+    }
+
+    public static void main(String[] args)
+    {
+        int ans=sub(17,8);
+        System.out.println(ans);
+
+    }
 }
